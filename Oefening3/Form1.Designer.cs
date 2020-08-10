ad39e2e0181c999de279bac9920319c112665787
Benjamin Vandevelde <benjamin_vandevelde@hotmail.com>
2020-08-10 13:29:07 +0200

    Oefening 3 afgewerkt

--- Oefening3/Form1.Designer.cs
+++ Oefening3/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbMijnLijst = new System.Windows.Forms.ListBox();
+            this.tbMijnTextBox = new System.Windows.Forms.TextBox();
+            this.btnMijnKnop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbMijnLijst
+            // 
+            this.lbMijnLijst.FormattingEnabled = true;
+            this.lbMijnLijst.Location = new System.Drawing.Point(32, 35);
+            this.lbMijnLijst.Name = "lbMijnLijst";
+            this.lbMijnLijst.Size = new System.Drawing.Size(137, 381);
+            this.lbMijnLijst.TabIndex = 0;
+            // 
+            // tbMijnTextBox
+            // 
+            this.tbMijnTextBox.Location = new System.Drawing.Point(191, 35);
+            this.tbMijnTextBox.Name = "tbMijnTextBox";
+            this.tbMijnTextBox.Size = new System.Drawing.Size(100, 20);
+            this.tbMijnTextBox.TabIndex = 1;
+            // 
+            // btnMijnKnop
+            // 
+            this.btnMijnKnop.Location = new System.Drawing.Point(191, 62);
+            this.btnMijnKnop.Name = "btnMijnKnop";
+            this.btnMijnKnop.Size = new System.Drawing.Size(100, 23);
+            this.btnMijnKnop.TabIndex = 2;
+            this.btnMijnKnop.Text = "Filter op text";
+            this.btnMijnKnop.UseVisualStyleBackColor = true;
+            this.btnMijnKnop.Click += new System.EventHandler(this.btnMijnKnop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMijnKnop);
+            this.Controls.Add(this.tbMijnTextBox);
+            this.Controls.Add(this.lbMijnLijst);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbMijnLijst;
+        private System.Windows.Forms.TextBox tbMijnTextBox;
+        private System.Windows.Forms.Button btnMijnKnop;
     }
 }
 
--- Oefening3/Form1.cs
+++ Oefening3/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,37 @@ namespace Oefening3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnMijnKnop_Click(object sender, EventArgs e)
+        {
+            VulLijst(tbMijnTextBox.Text);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VulLijst("");
+        }
+        public void VulLijst(string text)
+        {
+            lbMijnLijst.Items.Clear();
+            List<string> namenLijst = new List<string>();
+            using (StreamReader reader = new StreamReader("namen.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    namenLijst.Add(reader.ReadLine());
+                }
+            }
+            foreach (var item in namenLijst)
+            {
+                if (item.Contains(text))
+                {
+                    lbMijnLijst.Items.Add(item);
+                }
+
+            }
         }
     }
 }
